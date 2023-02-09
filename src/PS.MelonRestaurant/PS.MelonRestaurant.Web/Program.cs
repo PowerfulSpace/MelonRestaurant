a9fb8b827c0df64d19f89177e27e7a2c72ddff9b
Alexander <sany199574@gmail.com>
2023-02-10 02:51:00 +0300

    Добавил локализацию

--- src/PS.MelonRestaurant/PS.MelonRestaurant.Web/Program.cs
+++ src/PS.MelonRestaurant/PS.MelonRestaurant.Web/Program.cs
@@ -1,9 +1,9 @@
 using PS.MelonRestaurant.Web;
 using PS.MelonRestaurant.Web.Services;
 using PS.MelonRestaurant.Web.Services.IServices;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 // Add services to the container.
 
@@ -11,6 +11,22 @@ SD.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
 
 builder.Services.AddHttpClient<IProductService, ProductService>();
 builder.Services.AddScoped<IProductService, ProductService>();
+
+//Add Localization
+//--------------------------------------------------------------------------------
+RequestLocalizationOptions localizationOptions = new RequestLocalizationOptions();
+
+builder.Services.AddLocalization();
+var supportedCultures = new[]
+            {
+                new CultureInfo("en-US"),
+                new CultureInfo("es-ES")
+            };
+localizationOptions.SupportedCultures = supportedCultures;
+localizationOptions.SupportedUICultures = supportedCultures;
+localizationOptions.SetDefaultCulture("en-US");
+localizationOptions.ApplyCurrentCultureToResponseHeaders = true;
+//--------------------------------------------------------------------------------
 
 builder.Services.AddControllersWithViews();
 
@@ -22,6 +38,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseRequestLocalization(localizationOptions);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
